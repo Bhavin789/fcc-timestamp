e066abfe1afaf1e08b1306e1783a2b59c829865b
Bhavin Ag <bhavib.789@gmail.com>
2021-06-11 17:32:47 +0000

    chore: setup basic setup for whoami api

--- server.js
+++ server.js
@@ -2,17 +2,14 @@
 // where your node app starts
 
 // init project
+require('dotenv').config();
 var express = require('express');
 var app = express();
-var moment = require('moment');
-var bodyParser = require('body-parser')
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-app.use(bodyParser.urlencoded({ extended: "false" }));
-app.use(bodyParser.json());
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -28,33 +25,6 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-const isValidDate = (d) => ( Object.prototype.toString.call(d) === "[object Date]" && !isNaN(d.getTime()));
-
-const isValidNumber = (num) => !isNaN(Number(num));
-
-app.get("/api/:date?", function (req, res) {
-  const date = req.params.date;
-
-  if(!date){
-    return res.json({"unix":new Date().getTime(),"utc": new Date().toUTCString()});  
-  }
-
-  if(isValidNumber(date)){
-    const timestamp = Number(date);
-
-    if(!isValidDate(new Date(timestamp))){
-      return res.json({error: "Invalid Date"})
-    }
-
-    return res.json({"unix":new Date(timestamp).getTime(),"utc": new Date(timestamp).toUTCString()});
-  } 
-
-  if(!isValidDate(new Date(date))){
-      return res.json({error: "Invalid Date"})
-  }
-
-  return res.json({"unix":new Date(date).getTime(),"utc": new Date(date).toUTCString()});  
-});
 
 
 // listen for requests :)
